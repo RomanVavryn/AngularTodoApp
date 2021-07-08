bec6c9e15704348b5e1918c0dfa233b19d8db946
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-08 13:09:36 +0300

    created components for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {Page404Component} from "./page404/page404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', component: HomeComponent},
+  {path: '/todo', component: TodoListComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotepadComponent } from './notepad/notepad.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotepadComponent,
+    TodoListComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
