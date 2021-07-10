69c4ce027a5a65851eb68452c0b1374ee0585f97
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-10 15:00:18 +0300

    added todo data service

---
+++ src/app/todo-list/todos-data.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {TodosDataInterface} from "./todos-data.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosDataService {
+  todos: TodosDataInterface[] = [
+    {id: 1, title: 'todo title', description: 'do something', done: false},
+    {id: 2, title: 'todo app', description: 'finish todo app', done: false},
+    {id: 3, title: 'todo app 2', description: 'finish todo app', done: false},
+    {id: 4, title: 'todo app 3', description: 'finish todo app', done: false},
+    {id: 5, title: 'todo app 4', description: 'finish todo app', done: false},
+  ]
+
+  constructor() {
+  }
+
+  getTodos(): TodosDataInterface[] {
+    return this.todos
+  }
+
+  getLastId(): number {
+    return this.todos.length !== 0 ? this.todos[this.todos.length - 1].id : 1;
+  }
+
+  taskToDone(index: number | undefined): void {
+    // @ts-ignore
+    this.todos[index].done = !this.todos[index].done;
+  }
+
+  deleteTask(index: number | undefined): void {
+    if (typeof index === "number") {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  newTask(item: TodosDataInterface): void {
+    this.todos.push(item);
+  }
+
+}
