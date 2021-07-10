d742c7dfdbd2c14980bac645e9df262ff054aeb8
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-10 15:04:09 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {TodoListComponent} from "./todo-list/todo-list.component";
 import {Page404Component} from "./page404/page404.component";
-import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
+  {path: '', component: HomeComponent},
+  {path: '404', component: Page404Component},
   {path: 'todo', component: TodoListComponent},
-  {path: '**', component: Page404Component}
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NotepadComponent } from './notepad/notepad.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { Page404Component } from './page404/page404.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
+import { TodoNewItemComponent } from './todo-list/todo-new-item/todo-new-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NotepadComponent,
     TodoListComponent,
-    Page404Component
+    Page404Component,
+    TodoItemComponent,
+    TodoNewItemComponent
   ],
   imports: [
     BrowserModule,
