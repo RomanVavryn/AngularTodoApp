6bc75b72ffb3403ba68442493016fd10622cd003
Roman Vavrynkevych <roman.vavrun@gmail.com>
2021-07-19 18:49:30 +0300

    refactored task component implemented reactive form in edit task mode

--- src/app/todo-list/todo-item/todo-item.component.ts
+++ src/app/todo-list/todo-item/todo-item.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TodosDataInterface} from "../todos-data.interface";
 import {TodosDataService} from "../todos-data.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-todo-item',
@@ -12,15 +13,16 @@ export class TodoItemComponent implements OnInit {
   @Input() index: number | undefined;
 
   editMode: boolean = false;
-  editedTitle: string | undefined
-  editedDescription: string | undefined
-  CanSave: boolean = false;
+  editingForm: FormGroup | undefined;
+
   constructor(private todoDataService: TodosDataService) {
   }
 
   ngOnInit(): void {
-    this.editedTitle = this.todoItem?.title;
-    this.editedDescription = this.todoItem?.description;
+    this.editingForm = new FormGroup({
+      'editedTitle': new FormControl(this.todoItem?.title, [Validators.required, Validators.minLength(5)]),
+      'editedDescription': new FormControl(this.todoItem?.description, [Validators.required, Validators.minLength(5)])
+    })
   }
 
   taskDone(): void {
@@ -31,19 +33,14 @@ export class TodoItemComponent implements OnInit {
     this.todoDataService.deleteTask(this.index);
   }
 
-  canSaveChanges(): void {
-    if (this.editedTitle && this.editedDescription) {
-      this.CanSave = this.editedTitle.trim().length > 4 && this.editedDescription.trim().length > 4;
-    }
-  }
-
   saveChanges(): void {
     const updatedItem: TodosDataInterface = {
       id: this.todoItem?.id,
-      title: this.editedTitle,
-      description: this.editedDescription,
+      title: this.editingForm?.get('editedTitle')?.value,
+      description: this.editingForm?.get('editedDescription')?.value,
       done: this.todoItem?.done
     }
     this.todoDataService.updateItem(this.index, updatedItem);
   }
+
 }
